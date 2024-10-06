536b0132b5f47753f86f86fe59a7d2e3f07e4bde
jhosuapp <jhosuapvll@gmail.com>
2024-10-06 18:39:27 -0500

    feat: testing in useCounter

--- 02-hooks/src/hooks/useCounter.test.tsx
+++ 02-hooks/src/hooks/useCounter.test.tsx
@@ -1,16 +1,55 @@
-import { renderHook } from '@testing-library/react'
+import { renderHook, act } from '@testing-library/react'
 import { useCounter } from './useCounter';
     
 describe('UseCounter Hook', ()=>{
+
+    const initialValue = 10;
         
     it('should render default values', ()=>{
-        const { result } = renderHook( () => useCounter(10) );
+        const { result } = renderHook( () => useCounter(initialValue) );
         const { counter, increment, decrement, reset } = result.current;
 
-        expect(counter).toBe(10);
+        expect(counter).toBe(initialValue);
         expect(increment).toEqual(expect.any( Function ));
         expect(decrement).toEqual(expect.any( Function ));
         expect(reset).toEqual(expect.any( Function ));
     });
+
+    it('should increment intial value', ()=>{
+        const incrementAct = 2;
+        const { result } = renderHook( () => useCounter(initialValue) );
+        const { increment } = result.current;
+
+        act(()=>{
+            increment(incrementAct);
+        });
+
+        expect(result.current.counter).toBe(initialValue + incrementAct);
+    });
+
+    it('should decrement intial value', ()=>{
+        const decrementAct = 2;
+        const { result } = renderHook( () => useCounter(initialValue) );
+        const { decrement } = result.current;
+
+        act(()=>{
+            decrement(decrementAct);
+        });
+
+        expect(result.current.counter).toBe(initialValue - decrementAct);
+    });
+
+    it('should reset counter', ()=>{
+        const { result } = renderHook( () => useCounter(initialValue) );
+        const { reset, increment } = result.current;
+
+        act(()=>{
+            increment(2);
+            reset();
+        });
+
+        expect(result.current.counter).toBe(initialValue);
+    });
+
     
 });
