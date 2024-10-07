175701c1f1cee2c833340c787a7bf8630a88d959
jhosuapp <jhosuapvll@gmail.com>
2024-10-06 19:02:57 -0500

    feat: tests in useForm

---
+++ 02-hooks/src/hooks/useForm.test.tsx
@@ -0,0 +1,64 @@
+import { renderHook, act } from '@testing-library/react'
+import { useForm } from './useForm';
+    
+describe('UseForm Hooks', ()=>{
+
+    const initialValue = {
+        username: '',
+        email: '',
+        password: ''
+    }
+        
+    it('should render hook', ()=>{
+        const { result } = renderHook(()=> useForm(initialValue));
+
+        expect(result.current).toEqual({
+            HandleOnChange: expect.any( Function ),     
+            form: initialValue,
+            HandleOnReset: expect.any( Function ) 
+        });
+    });
+
+    it('should modify name', ()=>{
+        const newValue = 'Jhosua'
+        const { result } = renderHook(()=> useForm(initialValue));
+        const { HandleOnChange } = result.current;
+
+        act(()=>{
+            const mockEvent = {
+                target: {
+                    name: 'username',
+                    value: newValue
+                }
+            } as React.ChangeEvent<HTMLInputElement>; // Castea el objeto a ChangeEvent
+    
+            HandleOnChange(mockEvent);
+        });
+
+        expect(result.current.form.username).toBe(newValue);
+
+    });
+
+    it('should reset form', ()=>{
+        const newValue = 'Jhosua'
+        const { result } = renderHook(()=> useForm(initialValue));
+        const { HandleOnChange, HandleOnReset } = result.current;
+
+        act(()=>{
+            const mockEvent = {
+                target: {
+                    name: 'username',
+                    value: newValue
+                }
+            } as React.ChangeEvent<HTMLInputElement>; // Castea el objeto a ChangeEvent
+    
+            HandleOnChange(mockEvent);
+            HandleOnReset();
+        });
+
+        expect(result.current.form.username).toBe(initialValue.username);
+
+    });
+
+    
+});
